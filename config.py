5224b839cccf5f5be9e58fa5f480a948026151bd
Maximilian-vw <ahibglow73@gmail.com>
2024-11-14 02:39:58 +0700

    Update config.py with new changes

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 
 # Konfigurasi database MySQL dan JWT
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://pwl:pwl123@localhost:3306/db_apotek_api'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://pwl:pwl123@localhost:3306/db_apotek_api'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://flaskapi_usguessrun:b75649cf5f9200d1ea83d22591c4eb312c6a4f4b@styc1.h.filess.io:3305/flaskapi_usguessrun'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'WaHyUDi078'  # Key untuk JWT token
 
